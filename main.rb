e29802985db7afa81c477b5ac05441d60cd49cdf
tatsumu <t.m.555.tennis@gmail.com>
2015-09-16 18:03:54 +0900

    lesson5

--- main.rb
+++ main.rb
@@ -8,20 +8,8 @@ ActiveRecord::Base.establish_connection(
 class Post < ActiveRecord::Base
 end
 
-#post = Post.new(:title => "title1", :body => "hello1")
 
-=begin
-post = Post.new
-post.title="title2"
-post.body="hello2"
-
-=end
-=begin
-post =Post.new do |p|
-	p.title="title3"
-	p.body="hello3"
-end
-=end
-
-Post.create(:title => "title4", :body => "hello4")
-p Post.all
+# p Post.first
+# p Post.find(3)
+# p Post.find_by_title("title2")
+p Post.find_by_title_and_id("title2",2)
