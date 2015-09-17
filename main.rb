1257211e29824d34b5d1cdc68442da6c6f809baa
tatsumu <t.m.555.tennis@gmail.com>
2015-09-17 10:53:57 +0900

    lesson9

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+#レコードの更新
 require 'active_record'
 
 ActiveRecord::Base.establish_connection(
@@ -9,8 +10,16 @@ ActiveRecord::Base.establish_connection(
 class Post < ActiveRecord::Base
 end
 
-# Post.where(:title => "title5").first_or_create
-Post.where(:title => "title6").first_or_create do |p|
-	p.body="hello6"
-end
-p Post.all
+post = Post.find(1)#idが1のものを参照
+
+=begin
+post.title="(new)title1"#titleを書き換え
+post.save
+=end
+
+# post.update_attribute(:title,"(new2)title1")#titleを(new2)title1に書き換え
+# post.update_attributes(:title=>"nnn",:body=>"hhh")#複数の書き換え
+Post.where(:id=>1...2).update_all(:title=>"nnn2",:body=>"hhh2")  
+
+p Post.first
+
